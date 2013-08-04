b375005b9343441f75449c6778b37960639a5026
Luke Barlow <luke.barlow@gmail.com>
2013-08-04 13:07:17 +0100

    edit encoding rounds to nearest frames (15 fps)

--- src/prong/history/editEncoding.js
+++ src/prong/history/editEncoding.js
@@ -5,15 +5,28 @@ module.exports = {
     parse : parse // convert from string to editList
 }
 
+var frameRate = 15;
+
+// round the time to the nearest frame, and to 3 decimal places
+function roundToFrame(time){
+    return d3.round(Math.round(time * frameRate) / frameRate,3);
+}
+
+// expands the number to a full decimal without rounding. This is necessary
+// for ffmpeg to get the splicing correct
+function unround(time){
+    return Math.round(time * frameRate) / frameRate;
+}
+
 function stringify(editList){
     var e = editList.map(function(edit, i){
-        var a = [edit.track, d3.round(edit.start,3)];
+        var a = [edit.track, roundToFrame(edit.start)];
         // we only record the end time if it's different from the start
         // time of the following edit, or if it's the last edit in the list
         if (edit.end){
             if ((i == (editList.length - 1))
                 || (edit.end != editList[i+1].start)){
-                a.push(d3.round(edit.end,3));
+                a.push(roundToFrame(edit.end));
             }
         }
         return a.join(',');
@@ -29,13 +42,13 @@ function parse(s){
     edits.forEach(function(editData){
         var b = editData.split(',');
         var track = parseInt(b[0]);
-        var start = parseFloat(b[1]);
+        var start = unround(parseFloat(b[1]));
         var edit = {
             'track' : track,
             'start' : start
         }
         if (b.length > 2){
-            edit.end = parseFloat(b[2])
+            edit.end = unround(parseFloat(b[2]));
         }
         editList.push(edit);
     });
